95e7d7c639765ac8ea215f1d584e7e58b0974c6c
aab-dii <mabdillahabdi600@gmail.com>
2024-10-08 10:20:51 +0800

    Update

--- MyNavigationDrawer/app/src/main/java/com/abdi/mynavigationdrawer/MainActivity.kt
+++ MyNavigationDrawer/app/src/main/java/com/abdi/mynavigationdrawer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.abdi.mynavigationdrawer
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -32,8 +33,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+                .setAction("Action") { Toast.makeText(this@MainActivity, "Halo ini action dari snackbar", Toast.LENGTH_SHORT).show()}
+                .show()
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
