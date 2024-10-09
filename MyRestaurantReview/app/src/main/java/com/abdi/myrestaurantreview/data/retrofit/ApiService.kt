0c62bd86c034f3ce0d7f2e1af0c62a931822e9a5
aab-dii <mabdillahabdi600@gmail.com>
2024-10-09 10:26:37 +0800

    First Commit

---
+++ MyRestaurantReview/app/src/main/java/com/abdi/myrestaurantreview/data/retrofit/ApiService.kt
@@ -0,0 +1,22 @@
+package com.abdi.myrestaurantreview.data.retrofit
+
+import com.abdi.myrestaurantreview.data.response.PostReviewResponse
+import com.abdi.myrestaurantreview.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
+}
