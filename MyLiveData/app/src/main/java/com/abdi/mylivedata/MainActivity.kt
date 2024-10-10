82ba460317388bd20684416f7edbf448e7cdf293
aab-dii <mabdillahabdi600@gmail.com>
2024-10-10 10:45:35 +0800

    First Commit

---
+++ MyLiveData/app/src/main/java/com/abdi/mylivedata/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.abdi.mylivedata
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.abdi.mylivedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        // Panggil fungsi untuk subscribe
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+        liveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
+    }
+}
+
