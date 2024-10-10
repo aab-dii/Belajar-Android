1487f75635bc3973bef0ed3e8d5650916507c71d
aab-dii <mabdillahabdi600@gmail.com>
2024-10-10 11:02:31 +0800

    Update: Activity KTX

--- MyRestaurantReview/app/build.gradle.kts
+++ MyRestaurantReview/app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
 dependencies {
 
+    implementation (libs.androidx.activity.ktx)
     implementation(libs.glide)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
