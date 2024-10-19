242fc9f9e6a80fa2c41ad7fb137dac74d5af0dd5
aab-dii <mabdillahabdi600@gmail.com>
2024-10-19 10:09:31 +0800

    First Commit

--- Tes/app/src/main/java/com/abdi/tes/DetailActivity.kt
+++ Tes/app/src/main/java/com/abdi/tes/DetailActivity.kt
@@ -1,35 +1,38 @@
 package com.abdi.tes
 
-import android.net.Uri // Pastikan kamu mengimpor Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.abdi.tes.databinding.ActivityDetailBinding
 import com.bumptech.glide.Glide
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+    private val detailViewModel: DetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val eventName = intent.getStringExtra(EXTRA_NAME)
-        val eventPhoto = intent.getStringExtra(EXTRA_PHOTO)
+        // Mendapatkan ID event dari Intent
+        val eventId = intent.getIntExtra("id", -1)
+        Log.d("EventsAdapter", "ID default di detail activity: $eventId")
 
-        Glide.with(this)
-            .load(eventPhoto) // eventPhoto adalah URL gambar
-            .into(binding.imgEvent)
+        // Mengambil detail event
+        detailViewModel.setID(eventId)
 
-        binding.tvTitle.text = eventName
-        // Mengonversi string ke Uri sebelum memanggil setImageURI
-        eventPhoto?.let {
-            binding.imgEvent.setImageURI(Uri.parse(it))
+        // Observasi perubahan pada eventDetail
+        detailViewModel.eventDetail.observe(this) { eventDetail ->
+            if (eventDetail != null && !eventDetail.error) {  // Memastikan tidak ada error
+                binding.tvTitle.text = eventDetail.event.name
+                Glide.with(this)
+                    .load(eventDetail.event.mediaCover)
+                    .into(binding.imgEvent)
+            } else {
+                Log.e("DetailActivity", "Error: ${eventDetail?.message}")
+            }
         }
-    }
-
-    companion object {
-        const val EXTRA_PHOTO = "extra_photo"
-        const val EXTRA_NAME = "extra_name"
     }
 }
