11909cd3abf51b77c38ea717dd61bb96793a75e6
aab-dii <mabdillahabdi600@gmail.com>
2024-10-24 14:12:10 +0800

    Update: Dark Mode

---
+++ DicodingEventApp/app/src/main/java/com/abdi/dicodingeventapp/ui/favorite/FavoriteViewModel.kt
@@ -0,0 +1,7 @@
+package com.abdi.dicodingeventapp.ui.favorite
+
+import androidx.lifecycle.ViewModel
+
+class FavoriteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ DicodingEventApp/app/src/main/java/com/abdi/dicodingeventapp/ui/setting/SettingFragment.kt
@@ -0,0 +1,54 @@
+package com.abdi.dicodingeventapp.ui.setting
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import com.abdi.dicodingeventapp.databinding.FragmentSettingBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+class SettingFragment : Fragment() {
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: SettingViewModel by viewModels {
+        ViewModelFactory(SettingPreferences.getInstance(requireContext().dataStore))
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val switchTheme: SwitchMaterial = binding.switchTheme
+
+        viewModel.getThemeSettings().observe(viewLifecycleOwner) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            viewModel.saveThemeSetting(isChecked)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
